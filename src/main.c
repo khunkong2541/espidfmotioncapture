9f3d4e79081b005652b4aaf685c05df4e159f3ff
khunkong2541 <67286203+khunkong2541@users.noreply.github.com>
2020-08-25 20:08:51 +0700

    Change Clk speed

--- src/main.c
+++ src/main.c
@@ -45,7 +45,7 @@ void app_main(void *ignore) {
 	conf.scl_io_num = PIN_CLK;
 	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
 	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-	conf.master.clk_speed = 100000;
+	conf.master.clk_speed = 400000;
 	ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
 	ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
 
